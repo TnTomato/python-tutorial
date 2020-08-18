35596dc90bcb726dd8db49d191072d96dd5ce828
TnTomato <474093103@qq.com>
2020-08-18 16:25:12 +0800

    fix an operator

--- Chapter4-ConditionalStatement/answers.py
+++ Chapter4-ConditionalStatement/answers.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 now = datetime.now().strftime('%H:%M:%S')  # fmt: 00:00:00
 
-if now <= '08:00:00':
+if now < '08:00:00':
     print('sleep')
 elif now < '18:00:00':
     print('work')
